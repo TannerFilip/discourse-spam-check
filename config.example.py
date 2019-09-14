83af9ebcf88d3696f578a1358237d36d05b60cca
tanner <tannerfilip@gmail.com>
2019-09-14 01:07:44 -0500

    initial commit of somewhat functional, but not yet usable, code

---
+++ config.example.py
@@ -0,0 +1,8 @@
+'''
+Enter the required information below, and then copy this file to config.py
+'''
+
+DISCOURSE_DOMAIN = 'https://discourse.aaaaaaa.com'  # Enter domain here without trailing /
+API_KEY = 'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb' # API key, from DISCOURSE_DOMAIN/admin/api/keys. 
+API_USERNAME = 'cccccccc' # the username of the account that'll be running this
+
