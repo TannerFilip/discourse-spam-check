478500d9b53e834feab105382ae657d2d639af8f
tanner <tannerfilip@gmail.com>
2019-09-14 12:49:48 -0500

    I guess that's what I get for using visual studio

--- config.example.py
+++ config.example.py
@@ -3,6 +3,6 @@ Enter the required information below, and then copy this file to config.py
 '''
 
 DISCOURSE_DOMAIN = 'https://discourse.aaaaaaa.com'  # Enter domain here without trailing /
-API_KEY = 'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb' # API key, from DISCOURSE_DOMAIN/admin/api/keys. 
+API_KEY = 'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb' # API key, from DISCOURSE_DOMAIN/admin/api/keys.
 API_USERNAME = 'cccccccc' # the username of the account that'll be running this
 
