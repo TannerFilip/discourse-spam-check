6944751749a58e3c2a31f801ba2892bb6cdaabda
tanner <tannerfilip@gmail.com>
2019-09-16 01:32:24 -0500

    cleanup code a bit

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 #!/usr/bin/python3
-import requests
-import config
-import json
-from colorama import init, Fore, Back, Style
-import webbrowser
-import sys
 import argparse
 import pprint
+import sys
+import webbrowser
 
+import requests
+from colorama import init, Fore
 
-init() # initialize colorama
+import config
+
+init()  # initialize colorama
 
 pp = pprint.PrettyPrinter(indent=4)
 
 dd = config.DISCOURSE_DOMAIN
-req_headers = {'Api-Key': config.API_KEY,'Api-Username': config.API_USERNAME,'Content-Type': 'application/json'}
+REQ_HEADERS = {'Api-Key': config.API_KEY,
+               'Api-Username': config.API_USERNAME,
+               'Content-Type': 'application/json'}
+
 
 def get_suspect_users():
-    pgNum = 1
+    """
+    Get a list of suspect users.
+    Discourse defines this as not having much "read time" or many "read posts".
+
+    """
+    pg_num = 1
     suspect_users_full = {}
     suspect_len = 0
     i = 0
@@ -26,14 +34,16 @@ def get_suspect_users():
     while execute:
         # Grab the admin page of suspect users, and convert it to json.
         # Requests automatically turns the json it receives into a list.
-        # Discourse only shows a certain number on each page, so this will just loop until it gets to an empty page.
-        suspect_users = requests.get(dd+'/admin/users/list/suspect.json?page='+str(pgNum), headers=req_headers).json()
+        # Discourse only shows a few on each page so this just loops until it gets to a blank page.
+        suspect_users = requests.get(
+            dd + '/admin/users/list/suspect.json?page=' + str(pg_num),
+            headers=REQ_HEADERS).json()
 
-        for usrs in suspect_users: # there's probably a cleaner way to do this.
+        for usrs in suspect_users:  # there's probably a cleaner way to do this.
             suspect_users_full[i] = suspect_users[i]
             i += 1
 
-        pgNum += 1
+        pg_num += 1
         suspect_len = len(suspect_users)
         if suspect_len <= 0:
             execute = False
@@ -41,41 +51,48 @@ def get_suspect_users():
 
 
 def get_silenced_users():
-    pgNum = 1
+    """Same as previous, but for silenced users."""
+
+    pg_num = 1
     silenced_users_full = {}
-    silenced_len = 0
     i = 0
 
     execute = True
     while execute:
-        silenced_users = requests.get(dd+'/admin/users/list/silenced.json?page='+str(pgNum), headers=req_headers).json()
+        silenced_users = requests.get(
+            dd + '/admin/users/list/silenced.json?page=' + str(pg_num),
+            headers=REQ_HEADERS).json()
 
         for usrs in silenced_users:
             silenced_users_full[i] = silenced_users[i]
             i += 1
 
-        pgNum += 1
+        pg_num += 1
         silenced_len = len(silenced_users)
         if silenced_len <= 0:
             execute = False
     return silenced_users_full
 
 
-
 def delete_user(scan_username, scan_userid):
+    """
+    1.Asks for confirmation and if accepted
+    2.if accepted delete a user, blocking their email, IP, and any URLs they posted
+    """
+
     print('Really delete '+scan_username+'? There is no undo!')
     print('Type \'y\' and press return to delete.')
     confirmation = input()
     if confirmation.lower() == 'y':
         try:
-            requests.delete(dd+'/admin/users/'+scan_userid+'.json', headers=req_headers, params={
-                'context':'Determined to be a spammer by '+config.API_USERNAME+' (using discourse-spam-check)',
-                'block_email':'true',
-                'block_urls':'true',
-                'block_ip':'true',
-                'delete_posts':'true'
+            requests.delete(dd + '/admin/users/' + scan_userid + '.json', headers=REQ_HEADERS, params={
+                'context': 'Determined to be a spammer by ' + config.API_USERNAME + ' (using discourse-spam-check)',
+                'block_email': 'true',
+                'block_urls': 'true',
+                'block_ip': 'true',
+                'delete_posts': 'true'
                 })
-        except:
+        except KeyError:
             print('Unable to delete ' + scan_username)
         else:
             print('Deleted ' + scan_username)
@@ -84,6 +101,7 @@ def delete_user(scan_username, scan_userid):
 
 
 def get_user_action(scan_username, scan_userid):
+    """ Prompt for an input on what to do with each user. """
 
     while True:
         user_action = input(': ')
@@ -91,7 +109,6 @@ def get_user_action(scan_username, scan_userid):
             break
         elif user_action.lower() == 'd':
             delete_user(scan_username, scan_userid)
-            break
             break
         elif user_action.lower() == 'o':
             webbrowser.open_new_tab(dd+'/u/'+scan_username)
@@ -101,18 +118,22 @@ def get_user_action(scan_username, scan_userid):
             sys.exit(0)
         else:
             print(Fore.RED + 'Invalid command.' + Fore.RESET)
-            pass
 
     return user_action
 
 
-
 def scan_suspect_users():
-    suspect_users = get_suspect_users() # have to make the list first
+    """
+    1. Scans suspect user list
+    2. Prints in terminal the username and bio
+    3. Gives options on what to do next
+    """
+
+    suspect_users = get_suspect_users()  # have to make the list first
     for usrs in suspect_users:
         scan_username = suspect_users[usrs]['username']
         scan_userid = str(suspect_users[usrs]['id'])
-        scan_user = requests.get(dd+'/u/'+scan_username+'.json', headers=req_headers).json()
+        scan_user = requests.get(dd + '/u/' + scan_username + '.json', headers=REQ_HEADERS).json()
         print(Fore.RED + 'Found user: ' + scan_username + Fore.RESET)
         print('User ID: ' + scan_userid)
         print('User bio: ')
@@ -124,21 +145,21 @@ def scan_suspect_users():
         get_user_action(scan_username, scan_userid)
 
 
-
 def scan_silenced_users():
+    """Same as above, but for silenced users."""
+
     silenced_users = get_silenced_users()
     for usrs in silenced_users:
         scan_username = silenced_users[usrs]['username']
         scan_userid = str(silenced_users[usrs]['id'])
-        scan_user = requests.get(dd+'/u/'+scan_username+'.json', headers=req_headers).json()
+        scan_user = requests.get(dd + '/u/' + scan_username + '.json', headers=REQ_HEADERS).json()
         print(Fore.RED + 'Found user: ' + scan_username + Fore.RESET)
         print('User ID: ' + scan_userid)
         print('User bio: ')
         try:
             print(str(scan_user['user']['bio_raw']))
-        except:
+        except KeyError:
             print('User has no bio.')
-            pass
 
         print(Fore.YELLOW + 'What would you like to do?')
         print('[S]kip, [d]elete and block IP, [o]pen in browser, [q]uit' + Fore.RESET)
@@ -146,11 +167,9 @@ def scan_silenced_users():
         get_user_action(scan_username, scan_userid)
 
 
-
-# scan_suspect_users()
-
 parser = argparse.ArgumentParser(prog="Discourse Spam Checker")
-parser.add_argument('--type', '-t', help='Specify which user list to scan. Defaults to \'all\'', choices=['all','suspect','silenced'], default='all')
+parser.add_argument('--type', '-t', help='Specify which user list to scan. Defaults to \'all\'',
+                    choices=['all', 'suspect', 'silenced'], default='all')
 
 args = parser.parse_args()
 
